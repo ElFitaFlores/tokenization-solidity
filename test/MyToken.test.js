43299d882f628f8f55e10145c15096189768bedd
Rafael Flores <fita500@gmail.com>
2021-10-27 17:21:22 -0600

    Add tests for token sale

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -1,13 +1,7 @@
 const Token = artifacts.require('MyToken')
 
-var chai = require('chai')
+const chai = require('./setupChai')
 const BN = web3.utils.BN
-const chaiBN = require('chai-bn')(BN)
-chai.use(chaiBN)
-
-var chaiAsPromised = require('chai-as-promised')
-chai.use(chaiAsPromised)
-
 const expect = chai.expect
 
 require('dotenv').config({path: '../.env'})
@@ -35,11 +29,11 @@ contract('Token test', async (accounts) => {
         expect(instance.balanceOf(recipient)).to.eventually.be.a.bignumber.equal(new BN(sendTokens))
     })
 
-    it('is not possible to send more tokens than available in total', async () => {
-        let instance = this.myToken
-        let balanceOfDeployer = await instance.balanceOf(deployerAccount)
+    // it('is not possible to send more tokens than available in total', async () => {
+    //     let instance = this.myToken
+    //     let balanceOfDeployer = await instance.balanceOf(deployerAccount)
 
-        expect(instance.transfer(recipient, new BN(balanceOfDeployer + 1))).to.be.rejected
-        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceOfDeployer)
-    })
+    //     expect(instance.transfer(recipient, new BN(balanceOfDeployer + 1))).to.be.rejected
+    //     expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(balanceOfDeployer)
+    // })
 })
---
+++ test/MyTokenSale.test.js
@@ -0,0 +1,17 @@
+const TokenSale = artifacts.require('MyTokenSale')
+const Token = artifacts.require('MyToken')
+
+const chai = require('./setupChai')
+const BN = web3.utils.BN
+const expect = chai.expect
+
+require('dotenv').config({path: '../.env'})
+
+contract('TokenSale test', async (accounts) => {
+    const [deployerAccount, recipient, anotherAccount] = accounts
+
+    it('should not have any tokens in my deployerAccount', async ()=> {
+        let instance = await Token.deployed()
+        expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(new BN(0))
+    })
+})
