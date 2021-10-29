a48e3560d527b7897f0351614b69b80a90dc306b
Rafael Flores <fita500@gmail.com>
2021-10-29 17:12:19 -0600

    Add token sale test

--- test/MyTokenSale.test.js
+++ test/MyTokenSale.test.js
@@ -1,7 +1,7 @@
 const TokenSale = artifacts.require('MyTokenSale')
 const Token = artifacts.require('MyToken')
 
-const chai = require('./setupChai')
+const chai = require('./setupChai.js')
 const BN = web3.utils.BN
 const expect = chai.expect
 
@@ -10,7 +10,7 @@ require('dotenv').config({path: '../.env'})
 contract('TokenSale test', async (accounts) => {
     const [deployerAccount, recipient, anotherAccount] = accounts
 
-    it('should not have any tokens in my deployerAccount', async ()=> {
+    it('should not have any tokens in my deployerAccount', async () => {
         let instance = await Token.deployed()
         expect(instance.balanceOf(deployerAccount)).to.eventually.be.a.bignumber.equal(new BN(0))
     })
